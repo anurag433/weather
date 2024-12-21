9fb489cd1c8c02c8a703237002da449e1ea18b01
Anurag Singh <anurag504singh@gmail.com>
2024-12-22 01:10:33 +0530

    update

--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse
 
-# Create your views here.
+import requests
+import datetime
+
+def home(request):
+
+    if 'city' in requests.POST:
+        city = request.POST['city']
+
+    else :
+        city = 'Delhi'
+
+    url  = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=ee91c2932fa3e6b8a44818fb810ed332'
+
+    PARAMS = {'units':'metric'}
+
+    data = request.get(url,PARAMS).json()
+    
+    return render (request ,'index.html')
