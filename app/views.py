c1db0d5dc9d6c91afed55aaa53f81dfd329804bd
Anurag Singh <anurag504singh@gmail.com>
2024-12-22 10:21:17 +0530

    api_connect

--- app/views.py
+++ app/views.py
@@ -5,16 +5,22 @@ import datetime
 
 def home(request):
 
-    if 'city' in requests.POST:
-        city = request.POST['city']
-
-    else :
-        city = 'Delhi'
-
+    if 'city' in request.POST:
+         city = request.POST['city']
+    else:
+         city = 'indore'     
+    
     url  = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=ee91c2932fa3e6b8a44818fb810ed332'
 
-    PARAMS = {'units':'metric'}
+    params = {'units':'metric'}
 
-    data = request.get(url,PARAMS).json()
-    
-    return render (request ,'index.html')
+    data = requests.get(url, params=params).json()
+
+
+    description = data['weather'][0]['description']
+    icon = data['weather'][0]['icon']
+    temp = data['main']['temp']
+
+    day = datetime.date.today()
+
+    return render (request ,'index.html',{'description':description,'icon':icon,'temp':temp,'day':day,'city':city})
