0a8497bd6fcaab0e21a8a83fdda1f48a9fdaf8c7
Anurag Singh <anurag504singh@gmail.com>
2024-12-28 23:20:55 +0530

    updated

--- weather/settings.py
+++ weather/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-3la_wxuj%z=$xs=ju+#4fw(aw8&f!c*k5nwx9hfuvz*)$0eant
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
